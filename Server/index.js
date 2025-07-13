2c9a8952916168fdd0b33b7f6fe8ba24ef66fd8f
nikhil sharma <nikhil_sharma@appstle.com>
2025-07-13 21:07:20 +0530

    be-start

---
+++ Server/index.js
@@ -0,0 +1,35 @@
+import http from 'http';
+import fs from 'fs';
+import ExcelJS from'exceljs';
+const workbook = new ExcelJS.Workbook();
+const worksheet = workbook.addWorksheet('Log Data');
+
+worksheet.columns = [
+    { header: 'Timestamp', key: 'timestamp', width: 20 },
+    { header: 'Level', key: 'level', width: 10 },
+    { header: 'Message', key: 'message', width: 50 },
+    { header: 'Source', key: 'source', width: 20 },
+    {header: 'Req_url', key: 'req_url', width: 20 },
+];
+
+const myServer = http.createServer((req, res) => {
+    // const log = `${Date.now()} ${req.url}`;
+    const logData =   [{ timestamp: new Date(), level: 'INFO', message: 'User logged in data', source: 'AuthService', req_url: req.url }];
+    worksheet.addRows(logData);
+    // fs.appendFile('./serverLogs.exe', log,(err,data)=>{
+    //     res.end('Hello World!');
+    // })
+    workbook.xlsx.writeFile('logs.xlsx')
+        .then(() => {
+            console.log('Excel file created successfully!');
+        })
+        .catch(err => {
+            console.error('Error creating Excel file:', err);
+        });
+    res.end('Hello World!');
+})
+
+
+myServer.listen(8000, ()=>{
+    console.log('Server is running');
+});
